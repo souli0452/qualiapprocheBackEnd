915c23b73555db59284a54885acfd7029d728784
unknown <sofianobambore9@gmail.com>
2025-06-11 14:22:03 +0000

    update

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { BlockUI } from 'primeng/blockui';
 import { ProgressSpinner } from 'primeng/progressspinner';
 import { FeaturesService } from '../../services/feature-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-loader',
     templateUrl: './loader.component.html',
-    imports: [BlockUI, ProgressSpinner],
+    imports: [BlockUI, ProgressSpinner,CommonModule],
     styleUrl: './loader.component.scss'
 })
 export class LoaderComponent implements OnInit {
